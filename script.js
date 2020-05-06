9063e2cf187f1ffbbcb44405989a71a535af0ead
Nikolay Krivosheev <krivosheevnikolay1@yandex.ru>
2020-05-06 12:33:52 +0500

    plantMe

---
+++ script.js
@@ -0,0 +1,6 @@
+let intro = document.querySelector ('.intro')
+function introUp() {
+  intro.style.marginTop = -60 + 'px';
+  intro.style.color = 'rgb(138, 221, 138)';
+}  
+setTimeout(introUp, 100);
