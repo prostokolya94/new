d11a0e533516dcd9ed41eb6e27365377ce3d65cc
Nikolay Krivosheev <krivosheevnikolay1@yandex.ru>
2020-05-10 10:59:03 +0500

    plantMe2

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
+//сдвиг лого вверх при входе в ЛК
 let intro = document.querySelector ('.intro')
 let garden = document.querySelector ('.garden')
 function introUp() {
   intro.style.marginTop = -60 + 'px';
-  intro.style.color = 'rgb(138, 221, 138)';
+  intro.style.color = 'rgb(45, 51, 34)';
   garden.style.marginTop = 110 + 'px';
 }  
-setTimeout(introUp, 100);
+setTimeout(introUp, 200);
+
+
+//база растений
+const pullOfPlants = [
+  { 
+    id: 1, 
+    name:'Комнатная фиалка',
+    nameInLatin: 'Saintpaulia',
+    timeoutOfWater: 72, /*интервал полива, часов*/
+    maxTemp: 24,
+    minTemp: 15,
+    lightMode: 5,  /*1-темнота, 5-рассеянный свет, 10-прямой свет*/
+    lightTime: 14, /*световой день, часов*/
+  }
+]
+alert (pullOfPlants);
